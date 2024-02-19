fd370a7e2ce7e0e425f392b47879fde27dad44e5
Alex Zhuk <89257358590@mail.ru>
2024-02-19 20:59:33 +0300

    Написал код первых трех задач

--- HM/obj/Debug/net8.0/HM.AssemblyInfo.cs
+++ HM/obj/Debug/net8.0/HM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a5798ef95b119e4bdb574691083e11f28b691a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
